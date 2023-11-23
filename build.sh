9280009bde1dd6dbb610ed4910b76e8fdc8373bd
cold_sharp <kgaikj2cu@icloud.com>
2023-11-23 16:26:54 +0800

    feat: compile minimized kernel to print text

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-cargo build --target thumbv7em-none-eabihf
+# cargo build --target thumbv7em-none-eabihf
+
+# if command -v cargo bootimage >/dev/null 2>&1; then
+#   echo "cargo bootimage is installed."
+# else
+#   echo "cargo bootimage is not installed."
+# fi
+
+cargo bootimage
