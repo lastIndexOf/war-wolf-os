e29722c998162491c19bfe5c5a8f64d86a1e9191
cold_sharp <kgaikj2cu@icloud.com>
2024-01-10 12:21:48 +0800

    chore: simple understand stack overflow & triple fault

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,19 @@ pub extern "C" fn _start() -> ! {
 
     println!("System initialized!");
 
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 12;
-    }
+    // 访问无效内存地址，触发 page fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 12;
+    // }
+
+    // 栈溢出，调用栈返回到栈底的 guard page
+    // 这个 guard page 实际上没有关联物理内存，触发 page fault
+    // fn stack_overflow() {
+    //     stack_overflow(); // 每一次递归都会将返回地址入栈
+    // }
+
+    // // 触发 stack overflow
+    // stack_overflow();
 
     // cargo test 会生成一个默认的启动函数 main。
     // 在 no_main 环境下不会自动调用，因此需要主动调用
